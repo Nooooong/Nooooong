ebbb8f910c9c9050db9279f4e701d83427d0519e
atlas <atlas9539@gmail.com>
2018-06-16 01:10:53 +0900

    no message

--- app/src/main/java/com/every/nongnong/activities/HomeActivity.java
+++ app/src/main/java/com/every/nongnong/activities/HomeActivity.java
@@ -28,6 +28,6 @@ public class HomeActivity extends AppCompatActivity {
 
         CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
         indicator.setViewPager(viewPager);
-        //reset
+        //reset1
     }
 }
