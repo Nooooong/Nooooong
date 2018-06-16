ce90ee1a15be1a75b0f8145415d3a9c6d71b1c7e
atlas <atlas9539@gmail.com>
2018-06-16 17:21:40 +0900

    [add]  로그인 테스트 액티비티 생성 및 테스트

--- app/src/main/java/com/every/nongnong/activities/LoginTest.java
+++ app/src/main/java/com/every/nongnong/activities/LoginTest.java
@@ -1,41 +1,29 @@
 package com.every.nongnong.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 import com.every.nongnong.R;
 
 /**
- * Created by Dongju on 2018. 6. 10..
+ * Created by Dongju on 2018. 6. 16..
  */
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginTest extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_logintest);
 
 
-        Button button = findViewById(R.id.loginTest);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.done).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                Intent intent = new Intent(LoginActivity.this ,LoginTest.class);
-                startActivity(intent);
-
-
+                finish();
             }
         });
-
-
-
-
     }
 }
