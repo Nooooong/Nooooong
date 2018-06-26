20e2e9c48240ece9d9738d72b795874b4834a37d
LG-PC\LG <wlsgud419@gmail.com>
2018-06-26 15:39:12 +0900

    [add]인터넷 퍼미션, HttpConnection 클래스, 예제(테스트 시 네트워크 주소 변경 필)

--- app/src/main/java/com/every/nongnong/activities/LoginActivity.java
+++ app/src/main/java/com/every/nongnong/activities/LoginActivity.java
@@ -7,17 +7,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import android.content.ContentValues;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
 import com.every.nongnong.R;
+import com.every.nongnong.application.RequestHttpConnection;
 
 /**
  * Created by Dongju on 2018. 6. 10..
  */
 
 public class LoginActivity extends AppCompatActivity {
+
+    // web_output 텍스트 뷰 선언
+    public TextView HttpConnectionView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //--------------http URL의 결과값을 TextView로 출력 -----------------
+        // web_output의 id를 참조 받아옴
+        HttpConnectionView = (TextView) findViewById(R.id.web_output);
+        // URL 설정
+        String url = "http://172.30.1.23:55555/status";
+        // AsyncTask를 통해 HttpURLConnection을 수행
+        NetworkTask networkTask = new NetworkTask(url, null);
+        networkTask.execute();
+        //------------------------------------------------------------------
 
 
         Button button = findViewById(R.id.loginTest);
@@ -34,8 +54,35 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
-
-
     }
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result; // 요청 결과를 저장할 변수.
+            RequestHttpConnection requestHttpConnection = new RequestHttpConnection();
+            result = requestHttpConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+            HttpConnectionView.setText(s);
+        }
+    }
+
 }
